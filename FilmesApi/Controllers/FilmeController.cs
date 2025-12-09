2e0f13faac9e17ff28e6695077abb7fcde9e1db6
unknown <giovanna.vasques1101@hotmail.com>
2025-12-09 16:26:24 -0300

    feat: adicionando o endpoint PATCH para atualizações parciais - foi preciso adicionar uma nova library para trabalhar com o JSON e um novo Map para converter Filme para UpdateFilmeDto

--- FilmesApi/Controllers/FilmeController.cs
+++ FilmesApi/Controllers/FilmeController.cs
@@ -2,6 +2,7 @@
 using FilmesApi.Data;
 using FilmesApi.Dtos;
 using FilmesApi.Models;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FilmesApi.Controllers;
@@ -43,12 +44,31 @@ public class FilmeController : ControllerBase
     }
 
     [HttpPut("{id}")]
-    public IActionResult AtualizaFilme(int id, 
+    public IActionResult AtualizaFilme(int id,
         [FromBody] UpdateFilmeDto filmeDto)
     {
         var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
         if (filme is null) return NotFound();
         _mapper.Map(filmeDto, filme);
+        _context.SaveChanges();
+        return NoContent();
+    }
+
+    [HttpPatch("{id}")]
+    public IActionResult AtualizaFilme(int id, 
+        JsonPatchDocument<UpdateFilmeDto> patch)
+    {
+        var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
+        if (filme is null) return NotFound();
+
+        var filmeParaAtualizar = _mapper.Map<UpdateFilmeDto>(filme);
+        patch.ApplyTo(filmeParaAtualizar, ModelState);
+        if (!TryValidateModel(filmeParaAtualizar))
+        {
+            return ValidationProblem(ModelState);
+        }
+
+        _mapper.Map(filmeParaAtualizar, filme);
         _context.SaveChanges();
         return NoContent();
     }
